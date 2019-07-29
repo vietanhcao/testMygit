5ba535054dfad0d69256352419534f3eb62a362f
caoviet <vietanhcao1994@gmail.com>
2019-07-29 15:01:27 +0700

    edit log

--- test.js
+++ test.js
@@ -2,9 +2,9 @@
 console.log('test1');
 
 
-console.log('this fissle test')
+console.log('this fissle test');
 
-console.log('this file test1dfdf')
+console.log('this file test1dfdf123');
 
 let edit = 'ok';
 
